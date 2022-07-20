22667deb1f335e1de756f592fd3f4a74c028dfc8
William Dias <smdwilliam89@gmail.com>
2022-07-20 11:57:49 +0100

    Tipos de dados

---
+++ section.10/03/script.js
@@ -0,0 +1,17 @@
+const text = 'Javascript';
+const frase = 'Isso é o jogo';
+
+console.log(text + ' ' + frase);
+
+let total = 0;
+
+const compras = 30;
+const imposto = 10;
+
+total = compras + imposto;
+
+const dobro = total * 2;
+const dividir = total / 2;
+const desconto = total - 20;
+
+console.log(dobro, dividir, desconto);
