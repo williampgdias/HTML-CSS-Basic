87f22b3799685b52b0e8985c5a38f9bf6cafdb59
William Dias <smdwilliam89@gmail.com>
2022-07-20 17:02:13 +0100

    Galeria imagens

--- Final Project/js/script.js
+++ Final Project/js/script.js
@@ -24,3 +24,43 @@ function activeProduct(parameter) {
   }
 }
 parameters.forEach(activeProduct);
+
+//////////////////// FAQ ////////////////////
+
+const questions = document.querySelectorAll('.questions button');
+
+function activeQuestion(event) {
+  const question = event.currentTarget;
+  const controls = question.getAttribute('aria-controls');
+  const answer = document.getElementById(controls);
+
+  answer.classList.toggle('active');
+  const active = answer.classList.contains('active');
+  question.setAttribute('aria-expanded', active);
+}
+
+function questionsEvents(question) {
+  question.addEventListener('click', activeQuestion);
+}
+
+questions.forEach(questionsEvents);
+
+//////////////////// BIKES GALLERY ////////////////////
+
+const gallery = document.querySelectorAll('.bike-images img');
+const galleryContainer = document.querySelector('.bike-images');
+
+function changeImg(event) {
+  const img = event.currentTarget;
+  const media = matchMedia('(min-width: 900px)').matches;
+  console.log(media);
+  if (media) {
+    galleryContainer.prepend(img);
+  }
+}
+
+function galleryEvents(img) {
+  img.addEventListener('click', changeImg);
+}
+
+gallery.forEach(galleryEvents);
