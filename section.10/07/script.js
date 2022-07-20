b2654177cdf62bcb07c353b358ba370ec9c40e1a
William Dias <smdwilliam89@gmail.com>
2022-07-20 13:18:50 +0100

    Eventos 2

--- section.10/07/script.js
+++ section.10/07/script.js
@@ -8,12 +8,45 @@
 // console.log(curso.nome);
 // curso.completar();
 
-const ativar = document.querySelector('.ativar');
+// const ativar = document.querySelector('.ativar');
 
-function handleClick(event) {
-  console.log(event);
+// function handleClick(event) {
+//   console.log(event);
+// }
+
+// ativar.addEventListener('click', handleClick);
+
+// console.log(ativar);
+
+//////////////////// EVENTOS 2 ////////////////////
+
+// const botao = document.querySelector('.botao');
+
+// function mostrar() {
+//   const texto = document.querySelector('.texto');
+//   texto.classList.toggle('ativar');
+// }
+
+// botao.addEventListener('click', mostrar);
+
+//////////////////// WINDOW ////////////////////
+
+//////////////////// MOUSEOVER ////////////////////
+
+// function coordenadaMouse(event) {
+//   const coordenadas = {
+//     x: event.x,
+//     y: event.y,
+//   };
+//   console.log(coordenadas);
+// }
+
+// window.addEventListener('mousemove', coordenadaMouse);
+
+//////////////////// SCROLL ////////////////////
+
+function scroll() {
+  console.log(window.scrollY);
 }
 
-ativar.addEventListener('click', handleClick);
-
-console.log(ativar);
+window.addEventListener('scroll', scroll);
